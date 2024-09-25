bd1ccccfc14e18eab7695c65eb69a51e4cfb3ae3
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-25 11:42:00 -0300

    second commit

--- src/componentes/IconesHeader/index.js
+++ src/componentes/IconesHeader/index.js
@@ -1,16 +1,26 @@
-import './estilo.css'
+
 import perfil from '../../imagens/perfil.svg'
 import sacola from '../../imagens/sacola.svg'
+import styled from 'styled-components'
+
+const Icone = styled.li`
+    margin-right: 40px;
+    width: 25px;
+`
+const Icones = styled.ul`
+    display: flex;
+    align-items: center;
+`
 
 const icones = [perfil, sacola]
 
 function IconesHeader() {
     return (
-        <ul className='icones'>
-            { icones.map( (icone) => (
-              <li className='icone'><img src={icone}></img></li>
-            )) }
-        </ul>
+        <Icones>
+            {icones.map((Icone) => (
+                <li className='icone'><img src='{icone}'></img></li>
+            ))}
+        </Icones>
     )
 }
 
